90b0cbd90ee9fd7cd8d8b06fb29e91ee69540bb4
Carol Bradsen <carolbradsen@gmail.com>
2023-02-02 08:43:12 -0700

    complete meal.js tests.

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
+function nameMenuItem(itemName) {
+menuItemName = `Delicious ${itemName}`;
+return menuItemName;
+}
+
+function createMenuItem(name, price, type) {
+  return {
+  name: name,
+  price: price,
+  type: type
+  }
+}
+
+var ingredients = []
+
+function addIngredients(item, ingredientsList) {
+  if (ingredientsList.includes(item) === false) {
+      ingredientsList.push(item);
+  }
+  
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+function decreasePrice(price) {
+  return price *= 0.9
+}
+
+function createRecipe(title, ingredients, type) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
 
 }
 
-
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
