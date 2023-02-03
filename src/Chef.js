a9e9162e14f0515453f91b71e8948c387df9e1a9
Carol Bradsen <carolbradsen@gmail.com>
2023-02-03 13:50:57 -0700

    complete Chef extension

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,28 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurantName) {
+    this.name = name;
+    this.restaurant = restaurantName;
   }
+greetCustomer(name, value) {
+   if (value === true) {
+ return `Good morning, ${name}!`;
+   } else 
+   return `Hello, ${name}!`
+  
 }
+
+checkForFood(foodItem) {
+  var breakfast = this.restaurant.menus.breakfast;
+  for (var i = 0; i < 3; i++) {
+    if (foodItem === breakfast[i]) {
+       return `Yes, we're serving ${foodItem.name} today!`;
+    }
+  } 
+  return `Sorry, we aren't serving ${foodItem.name} today.`;
+}
+}
+
+
+
 
 module.exports = Chef;
